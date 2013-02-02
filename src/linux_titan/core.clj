b6f0c62a5046ee0765ff25b68372802c4d9d518c
Zack Maril <zack@zacharymaril.com>
2013-02-02 05:02:08 +0400

    Changing format of commit map

--- src/linux_titan/core.clj
+++ src/linux_titan/core.clj
@@ -3,4 +3,4 @@
 
 (def linux-src-dir "/Users/zackmaril/Projects/experiments/linux")
 (def rev-list (git/produce-rev-list linux-src-dir))
-(def commits (map git/format-commit-from-hash rev-list))
+(def commits (map git/parse-commit-from-hash rev-list))
--- src/linux_titan/git.clj
+++ src/linux_titan/git.clj
@@ -9,23 +9,37 @@
   (with-sh-dir "/Users/zackmaril/Projects/experiments/linux"
     (sh "git" "show" hash "--no-abbrev-commit" "--format=raw")))
 
-(defn produce-rev-list [dir]
+(defn produce-rev-list
+  "Given a source directory, this fn produes the entire list of
+  commits on the current branch."
+  [dir]
   (-> (with-sh-dir dir
         (sh "git" "rev-list" "--remotes" "-n 200"))
       (:out)
       (clojure.string/split #"\n")))
 
-(defn begins-with-n-spaces? [n s]
+(defn begins-with-n-spaces?
+  "Predicate asking whether s begins with n spaces."
+  [n s]
   (=  (take n (iterate (constantly \space) \space))
       (take n s)))
 
-(defn begins-with-this-string? [this s]
+(defn begins-with-this-string?
+  "Predicate asking whether this string begins with s string."
+  [this s]
   (= this (join (take (count this) s))))
 
-(defn str-drop [s t] (join (drop (count s) t)))
+(defn str-drop
+  "Dumbply drops t from s."
+  [s t] (join (drop (count s) t)))
 
 
-(defn parse-name [line]
+(defn parse-name
+  "Given a line like the following:
+   Linus Torvalds <torvalds@linux-foundation.org> 1359507746 +1100
+   It returns a map with the author's name, email, and date.
+   If the date isn't included, then it will be nil."
+  [line]
   (let [[name,rest-of-line] (split-with (complement #{\<}) line)
         [email,date-raw] (split-with (complement #{\>}) (join rest-of-line))
         date-str (join (take-while (partial not= \space)
@@ -37,7 +51,10 @@
      :email (trim (join (drop 1 email)))
      :date date}))
 
-(defn format-headers [[commit tree & others]]
+(defn parse-headers
+  "Given a list of the headers of the commit message, this parses and
+  formats into a map."
+  [[commit tree & others]]
   (let [parents (map (partial str-drop "parent ")
                      (filter (partial begins-with-this-string? "parent") others))
         
@@ -52,13 +69,13 @@
      :author author
      :committer   committer}))
 
-(defn format-change [line]
+(defn parse-change [line]
   line)
 
-(def commit-message-metainfo ["Signed-off-by" "Cc" "Reported-by" "Acked-by"
-                              "Tested-by" "Reviewed-by" "From"])
+(def commit-message-metainfo #{"Signed-off-by" "Cc" "Reported-by" "Acked-by"
+                               "Tested-by" "Reviewed-by" "From"})
 
-(defn format-body [body]
+(defn parse-body [body]
   (let [[message-lines diff-raw] (split-with (partial begins-with-n-spaces? 4) body)
         
         cleaned-message-lines (map clojure.string/trim message-lines)
@@ -67,18 +84,22 @@
                        selected-lines (filter
                                        (partial begins-with-this-string? meta)
                                        cleaned-message-lines)
-                       formatted-lines (map (comp parse-name
-                                                  (partial str-drop (str meta ": ")))
-                                            selected-lines)]
-                   {key formatted-lines}))        
+                       parsed-lines (map (comp parse-name
+                                               (partial str-drop (str meta ": ")))
+                                         selected-lines)]
+                   {key parsed-lines}))        
         metainfo (map finder commit-message-metainfo)
 
-        diffs (map format-change (drop 1 (split (join "\n" diff-raw ) #"\ndiff --git ")))]
-    (merge {:message cleaned-message-lines :diff diffs}
-           (apply merge metainfo))))
+        diffs (map parse-change (drop 1 (split (join "\n" diff-raw ) #"\ndiff --git ")))]
+    {:message (filter (complement commit-message-metainfo)
+                      cleaned-message-lines)
+     :diffs diffs
+     :people-mentioned (merge (apply merge metainfo))}))
 
-(defn format-commit-from-hash [hash]
+(defn parse-commit-from-hash
+  "From the hash, this goes and parses the commit into a usable
+  format." [hash]
   (let [raw-commit (-> hash show-commit :out)
         lines (clojure.string/split raw-commit #"\n")
         [headers, body] (split-with (complement (partial begins-with-n-spaces? 4)) lines)]    
-    (merge (format-headers headers) (format-body body))))
+    (merge (parse-headers headers) (parse-body body))))
