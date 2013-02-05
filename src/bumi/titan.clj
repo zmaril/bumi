620770af70e918959f0550a457d80675a2442dc4
Zack Maril <zack@zacharymaril.com>
2013-02-05 21:43:21 +0400

    Whitespace is going away.

--- src/bumi/titan.clj
+++ src/bumi/titan.clj
@@ -9,35 +9,28 @@
 (defn start []
   (println "Opening titan...")  
   (g/open graph-config)
-
   (println "Checking for keys and labels...")
   (g/transact!
    ;;Types
    (t/create-vertex-key-once :type String {:functional true})
-
    ;;Person types
    (t/create-vertex-key-once :name String
                              {:indexed true
-                              :functional true})
-   
+                              :functional true})   
    (t/create-vertex-key-once :email String
                              {:indexed true
                               :functional true})
-
    ;;Commit types
    (t/create-vertex-key-once :hash String
                              {:indexed true
                               :unique true
                               :functional true})
    (t/create-vertex-key-once :message String {:functional true})
-
    ;;File types
    (t/create-vertex-key-once :filename String
                              {:indexed true
                               :unique true
-                              :functional true})
-
-   
+                              :functional true})   
    ;;Labels
    ;;Commit -> Person
    (let [group (t/create-group 2 "mentioned")]
@@ -45,17 +38,13 @@
      (t/create-edge-label-once :Reported-by {:group group})
      (t/create-edge-label-once :Tested-by {:group group})
      (t/create-edge-label-once :Acked-by {:group group})
-     (t/create-edge-label-once :From {:group group}))
-   
-
+     (t/create-edge-label-once :From {:group group}))   
    ;;Person -> Commit
    (t/create-edge-label-once :committed)
    (t/create-edge-label-once :authored)
-   (t/create-vertex-key-once :committed {:functional true})
-   
+   (t/create-vertex-key-once :committed {:functional true})   
    ;;Commit -> commit
-   (t/create-edge-label-once :parent-of)
-   
+   (t/create-edge-label-once :parent-of)   
    ;;Commit -> File
    (t/create-edge-label-once :changed)
    (t/create-vertex-key-once :new? Boolean {:functional true})
