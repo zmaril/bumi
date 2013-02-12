d310799e788c36ab9a8146e48df793159295ee40
Zack Maril <zack@zacharymaril.com>
2013-02-12 14:24:58 +0400

    Config back in there

---
+++ src/bumi/config.clj
@@ -0,0 +1,17 @@
+(ns bumi.config)
+
+(def env (into {} (System/getenv)))
+
+(def graph-config
+  {:storage {:backend "berkeleyje"
+             :directory (or (env "BUMI_STORAGE_DIR")
+                            "/tmp/bumigraph/")}})
+
+(def git-root-dir (or (env "BUMI_GIT_DIR")
+                      "/Users/zackmaril/Projects/experiments/linux/"))
+
+(def debug (boolean (env "BUMI_DEBUG")))
+
+(defmacro debug-println [& body]
+  `(when ~debug
+     (println ~@body)))
